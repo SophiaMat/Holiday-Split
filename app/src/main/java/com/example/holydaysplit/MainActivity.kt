b905dddaee8df1eb997bdd1c4828d4add4bfab50
SophiaMat <sophiamatthis@outlook.de>
2020-10-02 20:28:04 +0200

    add starting money and set names

--- app/src/main/java/com/example/holydaysplit/MainActivity.kt
+++ app/src/main/java/com/example/holydaysplit/MainActivity.kt
@@ -7,14 +7,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     val person1 = Partner("Tom")
+    val person2 = Partner("Sophia")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setValuePartner(300.0)
+        setNames()
+        setStartingMoney(person1, 300.0)
+        setMoneyToSpend(person1, 300.0)
+        setStartingMoney(person2, 400.0)
+        setMoneyToSpend(person2, 400.0)
     }
 
-    private fun setValuePartner(moneyToSpend: Double) {
-        person1.moneyToSpend = moneyToSpend
-        currentStatus1.text = person1.moneyToSpend.toString() + "€"
+    private fun setNames(){
+        Partner1.text = person1.name
+        Partner2.text = person2.name
     }
+
+    private fun setMoneyToSpend(partner: Partner, moneyToSpend: Double) {
+        partner.moneyToSpend = moneyToSpend
+        currentStatus1.text = person1.moneyToSpend.toString() + "€"
+        currentStatus2.text = person2.moneyToSpend.toString() + "€"
+    }
+
+    private fun setStartingMoney(partner: Partner, startingMoney: Double) {
+        partner.startingMoney = startingMoney
+        startingValue1.text = person1.startingMoney.toString() + "€"
+        startingValue2.text = person2.startingMoney.toString() + "€"
+    }
+
 }
--- app/src/main/java/com/example/holydaysplit/Partner.kt
+++ app/src/main/java/com/example/holydaysplit/Partner.kt
@@ -4,4 +4,5 @@ import kotlin.properties.Delegates
 
 data class Partner(val name: String) {
     var moneyToSpend = 0.0
+    var startingMoney = 0.0
 }
