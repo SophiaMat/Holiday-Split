f2476a02a30fa62ff96b28e27936a5ceb0bbab0d
SophiaMat <sophiamatthis@outlook.de>
2020-10-11 21:12:39 +0200

    add button activity at startscreen

--- app/src/main/java/com/example/holydaysplit/StartDataActivity.kt
+++ app/src/main/java/com/example/holydaysplit/StartDataActivity.kt
@@ -2,10 +2,29 @@ package com.example.holydaysplit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_start_data.*
 
 class StartDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_data)
+    }
+
+    fun saveStartData(view: View){
+        var persons = mutableListOf<Partner>()
+        val person1 = Partner(Person1Name.text.toString())
+        val person2 = Partner(Person2Name.text.toString())
+        persons.add(person1)
+        persons.add(person2)
+        try{
+            persons[0].startingMoney = Person1Value.text.toString().toDouble()
+            persons[0].moneyToSpend = Person1Value.text.toString().toDouble()
+
+            persons[1].startingMoney = Person2Value.text.toString().toDouble()
+            persons[1].moneyToSpend = Person2Value.text.toString().toDouble()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
