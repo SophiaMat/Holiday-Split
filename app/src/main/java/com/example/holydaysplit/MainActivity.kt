7ab86c0c7053394d5cc2988403727fd278cf33e5
SophiaMat <sophiamatthis@outlook.de>
2020-10-12 14:29:55 +0200

    created new activity for spending money

--- app/src/main/java/com/example/holydaysplit/MainActivity.kt
+++ app/src/main/java/com/example/holydaysplit/MainActivity.kt
@@ -6,9 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_spend_money.*
 
 
-const val PERSONS = "com.example.holydaysplit.LIST"
 class MainActivity : AppCompatActivity() {
 
     var persons = mutableListOf<Partner>()
@@ -28,6 +28,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun getPersonsList(): MutableList<Partner> {
+        return persons
+    }
+
+    private fun setNames(){
+        Partner1.text = persons[0].name
+
+        Partner2.text = persons[1].name
+    }
+
     fun showStartdataScreen(){
         val intent = Intent(this, StartDataActivity::class.java).apply{
         }
@@ -36,18 +46,8 @@ class MainActivity : AppCompatActivity() {
 
     fun showMoneySpendingScreen(view: View){
         val intent = Intent(this, SpendMoneyActivity::class.java).apply{
-            //val passPersons = persons as ArrayList<Partner>
-            //putExtra(PERSONS, passPersons)
         }
         startActivity(intent)
-    }
-
-    private fun setNames(){
-        Partner1.text = persons[0].name
-        radioButton1.text = persons[0].name
-
-        Partner2.text = persons[1].name
-        radioButton2.text = persons[1].name
     }
 
     private fun setMoneyAppstart() {
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         setCurrentMoney()
     }
 
-    private fun setCurrentMoney(){
+    fun setCurrentMoney(){
         currentStatus1.text = persons[0].moneyToSpend.toString() + "€"
         currentStatus2.text = persons[1].moneyToSpend.toString() + "€"
         val total =  persons[0].moneyToSpend + persons[1].moneyToSpend
@@ -67,34 +67,6 @@ class MainActivity : AppCompatActivity() {
     fun deleteAllData(view: View){
         repository.deleteAllData(this)
         showStartdataScreen()
-    }
-
-    fun setNewMoneySpentValue(view: View) {
-        var money = moneySpent.text.toString()
-        var moneyparsed = money.toDoubleOrNull()
-        var id = radioGroup.checkedRadioButtonId
-        if(id!=-1){
-            if(moneyparsed != null){
-                val radio: RadioButton = findViewById<RadioButton>(id)
-                for(item in persons){
-                    if (item.name == radio.text.toString()){
-                        calculateNewCurrentStatus(item, moneyparsed)
-                    }
-                }
-            }
-        }
-        bringViewToStatusBack()
-        repository.saveData(persons, this)
-    }
-
-    private fun calculateNewCurrentStatus(person: Partner, money: Double){
-        person.moneyToSpend = person.moneyToSpend - money
-        setCurrentMoney()
-    }
-
-    private fun bringViewToStatusBack(){
-        moneySpent.setText("")
-        radioGroup.clearCheck()
     }
 
 }
--- app/src/main/java/com/example/holydaysplit/SpendMoneyActivity.kt
+++ app/src/main/java/com/example/holydaysplit/SpendMoneyActivity.kt
@@ -2,10 +2,48 @@ package com.example.holydaysplit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_spend_money.*
 
 class SpendMoneyActivity : AppCompatActivity() {
+
+    val repository = Repository()
+    var persons = mutableListOf<Partner>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spend_money)
+        setNamesRadioButton()
+    }
+
+    fun setNewMoneySpentValue(view: View) {
+        var money = moneySpent.text.toString()
+        var moneyparsed = money.toDoubleOrNull()
+        var id = radioGroup.checkedRadioButtonId
+        if(id!=-1){
+            if(moneyparsed != null){
+                val radio: RadioButton = findViewById<RadioButton>(id)
+                for(item in MainActivity().persons){
+                    if (item.name == radio.text.toString()){
+                        calculateNewCurrentStatus(item, moneyparsed)
+                    }
+                }
+            }
+        }
+        repository.saveData(MainActivity().persons, this)
+    }
+
+    private fun calculateNewCurrentStatus(person: Partner, money: Double){
+        person.moneyToSpend = person.moneyToSpend - money
+        MainActivity().setCurrentMoney()
+    }
+
+    private fun setNamesRadioButton(){
+        persons = repository.retrieveData(this) as MutableList<Partner>
+        radioButton1.text =  persons[0].name.toString()
+
+        radioButton2.text = persons[1].name
     }
 }
