600bb27cdcb909d904c358ce2681088b33978c9a
SophiaMat <sophiamatthis@outlook.de>
2020-11-09 20:24:50 +0100

    add timestamp

--- app/src/main/java/com/example/holydaysplit/Models/Item.kt
+++ app/src/main/java/com/example/holydaysplit/Models/Item.kt
@@ -1,5 +1,12 @@
 package com.example.holydaysplit.Models
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+
+import java.time.format.DateTimeFormatter
 
 data class Item (val name: String) {
     var price: Double? = 0.0
+    @RequiresApi(Build.VERSION_CODES.O)
+    val time = LocalDateTime.now()
 }
